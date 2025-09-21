64cc8095217881f1681b398dbdf733e5d87d4f9f
Kamari Isaac <kamariisaac6@gmail.com>
2025-09-21 00:09:54 -0400

    get specific todo api route

--- server/index.js
+++ server/index.js
@@ -34,6 +34,16 @@ app.get("/todos", async(req, res) => {
 });
 //get a todo
 
+app.get("/todos/:id", async(req,res) => {
+    try {
+        const {id} = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todo.rows[0])
+    }   catch (err) {
+        console.error(err.message);
+    }
+});
+
 //delete a todo
 
 app.listen(5000, () => {
