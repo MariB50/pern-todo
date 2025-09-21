3ba149223169a74131af05bdd56ce91349e72d8a
Kamari Isaac <kamariisaac6@gmail.com>
2025-09-21 00:03:11 -0400

    get all todos route

--- server/index.js
+++ server/index.js
@@ -24,7 +24,14 @@ try {
 })
 
 //get all todos
-
+app.get("/todos", async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    }   catch (err) {
+        console.error(err.message);
+    }
+});
 //get a todo
 
 //delete a todo
